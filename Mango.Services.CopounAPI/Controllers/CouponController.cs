43193d25d405c15d9b77a83a74e2c7a5fc5fccb0
BalajiSai2003 <balajisai2003@outlook.com>
2024-10-25 10:30:40 +0530

    Add project files.

---
+++ Mango.Services.CopounAPI/Controllers/CouponController.cs
@@ -0,0 +1,156 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Azure.Core.HttpHeader;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        private ResponseDTO _response;
+        private IMapper _mapper;
+
+        public CouponController(AppDbContext appDbContext, IMapper mapper)
+        {
+            this._appDbContext = appDbContext;
+            _mapper = mapper;
+            _response = new ResponseDTO();
+        }
+
+        [HttpGet]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> coupons = _appDbContext.Coupons.ToList();
+                if (coupons == null)
+                {
+                    _response.IsSuccess = false;
+                }
+                _response.Result = _mapper.Map<IEnumerable<CouponDTO>>(coupons);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetById/{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _appDbContext.Coupons.FirstOrDefault(c => c.CouponId == id);
+                if (coupon == null)
+                {
+                    _response.IsSuccess = false;
+                }
+
+                _response.Result = _mapper.Map<CouponDTO>(coupon); ;
+            }
+            catch (Exception ex)
+            {
+                _response.Result = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public object Get(string code)
+        {
+            try
+            {
+                Coupon? coupon = _appDbContext.Coupons.FirstOrDefault(c => c.CouponCode.ToLower() == code.ToLower());
+                if(coupon == null)
+                {
+                    _response.IsSuccess = false;
+                }
+                _response.Result = _mapper.Map<CouponDTO>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.Result = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDTO Post([FromBody] CouponDTO couponDTO)
+        {
+            try
+            {
+                Coupon coupon = _mapper.Map<Coupon>(couponDTO);
+                _appDbContext.Coupons.Add(coupon);
+                _appDbContext.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDTO>(coupon);
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+
+        }
+
+        [HttpPut]
+        public ResponseDTO Put([FromBody] CouponDTO couponDTO)
+        {
+            try
+            {
+                Coupon coupon = _mapper.Map<Coupon>(couponDTO);
+                _appDbContext.Coupons.Update(coupon);
+                _appDbContext.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDTO>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ResponseDTO Delete(int id)
+        {
+            try
+            {
+                Coupon coupon = _appDbContext.Coupons.FirstOrDefault(c => c.CouponId == id);
+                if (coupon == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.Message = "Coupon not found";
+                }
+                else
+                {
+                    _appDbContext.Coupons.Remove(coupon);
+                    _appDbContext.SaveChanges();
+                    _response.Result = _mapper.Map<CouponDTO>(coupon);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
---
+++ Mango.Services.CopounAPI/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "ABCD",
+                DiscountAmount = 20,
+                MinAmount = 10,
+            },
+            new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "EFGH",
+                DiscountAmount = 40,
+                MinAmount = 10,
+            });
+        }
+    }
+}
