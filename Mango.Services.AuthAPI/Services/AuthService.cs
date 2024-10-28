7a51eb5cda4fa7f642ff49fb6040c1f81ca00062
BalajiSai2003 <balajisai2003@outlook.com>
2024-10-28 12:31:47 +0530

    Add JWT configuration and AuthService implementation
    - Added JwtOptions class to hold JWT settings in Mango.Services.AuthAPI.Models.
    - Registered JwtOptions configuration in Program.cs using builder.Services.Configure<JwtOptions>.
    - Introduced AuthService class in Mango.Services.AuthAPI.Services to handle user registration and login.
    - Created IAuthService interface in Mango.Services.AuthAPI.Services.IServices to define AuthService contract.
    - Updated appsettings.json with ApiSettings section for JWT configuration.

---
+++ Mango.Services.AuthAPI/Services/AuthService.cs
@@ -0,0 +1,59 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.DTO;
+using Mango.Services.AuthAPI.Services.IServices;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext appDbContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _appDbContext = appDbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserDTO> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            ApplicationUser user = new()
+            {
+                UserName = registrationRequestDTO.Email,
+                Email = registrationRequestDTO.Email,
+                NormalizedEmail = registrationRequestDTO.Email.ToUpper(),
+                Name = registrationRequestDTO.Name,
+                PhoneNumber = registrationRequestDTO.PhoneNumber,
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDTO.Password);
+                if (result.Succeeded)
+                {
+                    var userToReturn = await _appDbContext.ApplicationUsers.FirstOrDefaultAsync(u => u.UserName == registrationRequestDTO.Email);
+                    return new UserDTO()
+                    {
+                        Name = userToReturn.Name,
+                        Email = userToReturn.Email,
+                        PhoneNumber = userToReturn.PhoneNumber,
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return new UserDTO();
+        }
+    }
+}
---
+++ Mango.Services.AuthAPI/Services/IServices/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.AuthAPI.Models.DTO;
+
+namespace Mango.Services.AuthAPI.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<UserDTO> Register(RegistrationRequestDTO registrationRequestDTO);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+
+    }
+}
