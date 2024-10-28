343813fe807171c451c482660e5711f8dc6f1d79
BalajiSai2003 <balajisai2003@outlook.com>
2024-10-28 12:06:43 +0530

    Add basic auth with Identity and update user model
    Introduced basic authentication functionality using ASP.NET Core Identity. Added `AuthAPIController` with `Register` and `Login` endpoints. Created `AppDbContext` for Identity setup and updated project file to include Identity package. Added migrations to create and configure Identity tables.
    
    Updated `ApplicationUser` to include `Name` property and added a migration to reflect this change. Added DTO classes for user authentication and registration. Configured `AppDbContext` and Identity services in `Program.cs`, including a method to apply migrations at runtime. Updated `appsettings.json` with SQL Server connection string and added authentication middleware to the HTTP request pipeline.

---
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    [Route("api/Auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
